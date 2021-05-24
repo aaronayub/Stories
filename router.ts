5dd0027cda2ec0d273835cf8d1b7ef0938c4d37a
Aaron Ayub <55106255+AaronAyub@users.noreply.github.com>
2021-05-24 18:55:09 -0400

    Partial work on getting registrations functional.

--- router.ts
+++ router.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import session from 'express-session'
-import sql = require("./sql")
+import bcrypt from 'bcrypt'
+import con from './sql'
+import { nextTick } from 'process'
 var router = express.Router()
 
 // Express-session variables must be declaration merged with typescript
@@ -9,7 +11,9 @@ declare module 'express-session' {
         username: string
     }
 }
+
 router.use(session({secret: "StoriesApp session secret",saveUninitialized: false, resave: false}))
+router.use(express.urlencoded({extended:false}))
 
 router.get('/login',(req,res,next)=>{
     if (req.session.username) {
@@ -29,7 +33,30 @@ router.get('/register',(req,res,next)=>{
         title: "Register"
     })
 })
-router.get('*',(req,res) => {
+router.post('/register',(req,res,next)=>{
+    var valid = true // User is eligible for registering
+    if (req.body.password.length < 6) valid = false // If the password isn't of the required length
+    else if (req.body.confirm !== req.body.password) valid = false // If the password and confirmed password don't match
+    con.query('SELECT username FROM users WHERE username = ?',
+    [req.body.username], (err,result)=>{
+        console.log(result)
+        console.log(con.escape(req.body.username))
+        if (result.length > 0) { // If the query returns a row, then the username is already taken
+            valid = false
+        }
+    })
+    if (valid) {
+        bcrypt.hash(req.body.password,10,(err,hash)=>{
+            con.query('INSERT INTO users (username,pass,account) VALUES (?,?,?)',
+            [req.body.username,hash,'user'], (err,result)=>{
+                if (err) console.log(err)
+                req.session.username = con.escape(req.body.username)
+                next()
+            })
+        })
+    }
+})
+router.all('*',(req,res) => {
     res.render('homepage',{
         username: req.session.username,
         title: "Stories App"
--- sql.ts
+++ sql.ts
@@ -1,23 +1,30 @@
 import mysql from 'mysql'
 
-var connected: boolean = false
-
 // Edit these parameters to authenticate to your own server if you are hosting this locally
 var con = mysql.createConnection({
     host: 'localhost',
     user: 'user',
-    password: 'password'
+    password: 'password',
+    database: 'storiesApp'
 })
-// Connect to the appropriate database
-con.connect((err)=>{
-    if (err) return
-    con.query("use storiesApp",(err)=>{
-        if (err) return
-        connected = true
-    })
-})
+con.connect()
 
+export default con
+// var connected: boolean = false
+// con.connect((err)=>{
+//     if (err) return
+//     con.query("use storiesApp",(err)=>{
+//         if (err) return
+//         connected = true
+//     })
+// })
 // Returns true if the database is successfully connected
-export function getConnected(): boolean {
-    return connected
-}
+// export function getConnected(): boolean {
+//     return connected
+// }
+
+
+// export function selectFrom(from: string, where: string, value: string) {
+//     var query = 'SELECT * FROM ? WHERE ? = ?'
+//     return con.query(query, [from,where,value])
+// }
