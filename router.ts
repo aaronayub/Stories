37dd21c4937c82a99d777a1327ef10d79a8301a0
Aaron Ayub <55106255+AaronAyub@users.noreply.github.com>
2021-05-24 15:48:20 -0400

    Added header partial. Made simple login and registration page templates.

--- router.ts
+++ router.ts
@@ -3,13 +3,36 @@ import session from 'express-session'
 import sql = require("./sql")
 var router = express.Router()
 
+// Express-session variables must be declaration merged with typescript
+declare module 'express-session' {
+    interface SessionData {
+        username: string
+    }
+}
+router.use(session({secret: "StoriesApp session secret",saveUninitialized: false, resave: false}))
 
-router.get('/', (req,res) => {
-    // if (req.session) {
-    //     req.session.test = "test"
-    // }
+router.get('/login',(req,res,next)=>{
+    if (req.session.username) {
+        next()
+    }
+    res.render('login',{
+        username: req.session.username,
+        title: "Log In"
+    })
+})
+router.get('/register',(req,res,next)=>{
+    if (req.session.username) {
+        next()
+    }
+    res.render('register',{
+        username: req.session.username,
+        title: "Register"
+    })
+})
+router.get('*',(req,res) => {
     res.render('homepage',{
-
+        username: req.session.username,
+        title: "Stories App"
     })
 })
 export default router
