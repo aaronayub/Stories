50aa8b41bebee8177babb60b67c6ea0a51f522d6
Aaron Ayub <55106255+AaronAyub@users.noreply.github.com>
2021-05-31 16:35:58 -0400

    Fixed bug with stylesheet not loading on stories page. Your Stories now displays story count.

--- controllers/stories.ts
+++ controllers/stories.ts
@@ -1,15 +1,33 @@
 import express from 'express'
 import renderPage from './renderPage'
+import con from './sql'
+import Story from '../interfaces/story'
 var router = express.Router()
 
-router.get('/',(req,res)=>{
+router.get('/',async (req,res)=>{
     if (!req.session.username) {
         req.session.output = "You have to be logged in to view your stories."
         res.redirect('/')
         return
     }
     
-    renderPage(req,res,'stories','Your Stories')
+    let stories: Story[] = []
+    const results : any = await con.promise().query("SELECT * FROM stories WHERE username = ?",
+    [req.session.username])
+    results[0].forEach((row: any)=>{
+        let story = {
+            id: row.id,
+            title: row.title,
+            brief: row.brief,
+            content: row.content,
+            username: row.username,
+            rating: row.rating,
+            created: row.created.toLocaleDateString()
+        }
+        stories.push(story)
+    })
+    console.log(stories)
+    renderPage(req,res,'stories','Your Stories',null,stories)
 })
 
 export default router
