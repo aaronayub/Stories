6b5830fb7fd7c4dcb7122d8df6df2b5b311e7056
Aaron Ayub <55106255+AaronAyub@users.noreply.github.com>
2021-06-05 16:21:16 -0400

    Reduced story brief character count to 256 characters. Fixed front-end restriction on username length in registration. Reduced size of ratings column in the table of all stories.

--- sql/initDatabase.sql
+++ sql/initDatabase.sql
@@ -6,7 +6,7 @@ USE storiesApp;
 
 /* All registered users */
 CREATE TABLE users (
-    username VARCHAR(64) NOT NULL PRIMARY KEY,
+    username VARCHAR(40) NOT NULL PRIMARY KEY,
     pass VARCHAR(256) NOT NULL,
     account VARCHAR(32) DEFAULT 'user',
     registered TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -17,9 +17,9 @@ Users may omit adding a short description, but can't submit blank stories. */
 CREATE TABLE stories (
     id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
     title VARCHAR(64) NOT NULL,
-    brief VARCHAR(512),
+    brief VARCHAR(256),
     content LONGTEXT NOT NULL,
-    username VARCHAR(64) NOT NULL,
+    username VARCHAR(40) NOT NULL,
     rating DECIMAL(3,2) DEFAULT 0.00,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -31,7 +31,7 @@ CREATE TABLE stories (
 /* All ratings for a given story, by a given user*/
 CREATE TABLE ratings (
     id INT UNSIGNED AUTO_INCREMENT NOT NULL,
-    username VARCHAR(64) NOT NULL,
+    username VARCHAR(40) NOT NULL,
     rating TINYINT NOT NULL,
     PRIMARY KEY (id, username),
     FOREIGN KEY (username) REFERENCES users (username)
@@ -45,7 +45,7 @@ CREATE TABLE ratings (
 /* Login tokens for persistent sessions */
 CREATE TABLE logins (
     token VARCHAR(128) NOT NULL PRIMARY KEY,
-    username VARCHAR(64) NOT NULL,
+    username VARCHAR(40) NOT NULL,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (username) REFERENCES users (username)
         ON DELETE CASCADE
