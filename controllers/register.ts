246ae5378ee2a28eb0c4d3bb385bb4bbc0bc6104
Aaron Ayub <55106255+AaronAyub@users.noreply.github.com>
2021-06-05 09:49:38 -0400

    Disallowed usernames that are too short, long, or have the " character. Story covers now also function as links to the story.

--- controllers/register.ts
+++ controllers/register.ts
@@ -16,6 +16,22 @@ router.post('/',async(req,res)=>{
     let valid: boolean = true // User is eligible for registering
     if (req.body.password.length < 6) valid = false // If the password isn't of the required length
     else if (req.body.confirm !== req.body.password) valid = false // If the password and confirmed password don't match
+    
+    // Disallow the use of a blank username or very long usernames
+    if (req.body.username.length < 1 || req.body.username.length > 40) {
+        req.session.output = "Username must be between 1 and 40 characters"
+        valid = false
+    }
+    // Disallow the / character, which breaks URLs
+    if (req.body.username.includes("/")) {
+        req.session.output = "Username cannot contain the \" character."
+        valid = false
+    }
+    // Avoid querying the database if the registration is already invalid
+    if (!valid) {
+        res.redirect('/register')
+        return
+    }
     let [rows] = await con.promise().query('SELECT username FROM users WHERE username = ?',
     [req.body.username])
     if (rows[0]) {
